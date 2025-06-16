4600cc2715a3e1bbf2c0a2f9cef09dd8ed13fd94
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2025-06-16 16:50:13 +0800

    增加下方功能区的导航

---
+++ Source/CraftSeal/MainNavigationSelector.cs
@@ -0,0 +1,23 @@
+﻿using CraftSeal.ViewModels;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace CraftSeal;
+
+internal partial class MainNavigationSelector : DataTemplateSelector
+{
+    public DataTemplate SessionTemplate { get; set; } = null!;
+
+    public DataTemplate OtherTemplate { get; set; } = null!;
+
+    protected override DataTemplate SelectTemplateCore(object item)
+    {
+        return item switch
+        {
+            SessionVM => SessionTemplate,
+            _ => OtherTemplate,
+        };
+    }
+
+    protected override DataTemplate SelectTemplateCore(object item, DependencyObject container) => SelectTemplate(item);
+}
---
+++ Source/CraftSeal/NavigationItemStub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace CraftSeal;
+
+internal class NavigationItemStub
+{
+    public Symbol Icon { get; set; }
+
+    public string Title { get; set; } = null!;
+}
