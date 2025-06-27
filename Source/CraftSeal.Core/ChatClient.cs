074c657727f6770351866cb52e5a62cf728cf184
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2025-06-27 12:38:21 +0800

    CancellationToken支持

--- Source/CraftSeal.Core/ChatClient.cs
+++ Source/CraftSeal.Core/ChatClient.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http.Json;
 using System.Net.ServerSentEvents;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 using CraftSeal.Api;
 
@@ -22,7 +23,7 @@ public class ChatClient(
 
     public void Dispose() => _httpClient.Dispose();
 
-    public async Task<ChatResponse> CompleteAsync(IEnumerable<ChatRequestMessage> messages)
+    public async Task<ChatResponse> CompleteAsync(IEnumerable<ChatRequestMessage> messages, CancellationToken cancellationToken = default)
     {
         var request = new ChatRequest
         {
@@ -31,14 +32,14 @@ public class ChatClient(
             Stream = false,
         };
 
-        using var response = await _httpClient.PostAsJsonAsync("v1/chat/completions", request, ChatApiSerializationContext.Default.ChatRequest).ConfigureAwait(false);
+        using var response = await _httpClient.PostAsJsonAsync("v1/chat/completions", request, ChatApiSerializationContext.Default.ChatRequest, cancellationToken).ConfigureAwait(false);
         var result = await response.EnsureSuccessStatusCode().Content
-            .ReadFromJsonAsync(ChatApiSerializationContext.Default.ChatResponse)
+            .ReadFromJsonAsync(ChatApiSerializationContext.Default.ChatResponse, cancellationToken)
             .ConfigureAwait(false);
         return result ?? throw new Exception("Top level null");
     }
 
-    public async IAsyncEnumerable<ChatResponseChunk> ChunkedCompleteAsync(IEnumerable<ChatRequestMessage> messages)
+    public async IAsyncEnumerable<ChatResponseChunk> ChunkedCompleteAsync(IEnumerable<ChatRequestMessage> messages, [EnumeratorCancellation] CancellationToken cancellationToken)
     {
         var request = new ChatRequest
         {
@@ -52,8 +53,8 @@ public class ChatClient(
             Content = JsonContent.Create(request, ChatApiSerializationContext.Default.ChatRequest)
         };
 
-        using var response = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
-        var responseStream = await response.EnsureSuccessStatusCode().Content.ReadAsStreamAsync().ConfigureAwait(false);
+        using var response = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+        var responseStream = await response.EnsureSuccessStatusCode().Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
 
         var reader = SseParser.Create(responseStream, (name, value) =>
         {
@@ -63,7 +64,7 @@ public class ChatClient(
             return JsonSerializer.Deserialize(value, ChatApiSerializationContext.Default.ChatResponseChunk);
         });
 
-        await foreach (var item in reader.EnumerateAsync().ConfigureAwait(false))
+        await foreach (var item in reader.EnumerateAsync(cancellationToken).ConfigureAwait(false))
             if (item.Data != null)
                 yield return item.Data;
     }
