897320d3bcd372e5372585391886e5db3568bd05
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2025-07-01 16:44:50 +0800

    引入SettingsCard控件

---
+++ Source/CraftSeal/ViewModels/SettingsVM.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CraftSeal.ViewModels;
+
+internal partial class SettingsVM : ObservableObject
+{
+    [ObservableProperty]
+    private string _apiKey = string.Empty;
+}
