f20a0b8e4187c85193f340860b9b67bf558c7071
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2025-07-04 13:22:54 +0800

    创建Chat client

--- Source/CraftSeal/Services/ChatClientService.cs
+++ Source/CraftSeal/Services/ChatClientService.cs
@@ -2,10 +2,16 @@
 
 internal class ChatClientService
 {
-    public string ApiKey { get; private set; } = "Dummy API Key";
+    private ChatClient? _chatClient;
+
+    public bool Initialized => _chatClient != null;
+
+    public string ApiKey { get; private set; } = string.Empty;
 
     public void UpdateApiKey(string apiKey)
     {
-
+        ApiKey = apiKey;
+        _chatClient = new ChatClient("https://api.deepseek.com/", apiKey);
+        // No disposal for old client, to keep old request alive
     }
 }
